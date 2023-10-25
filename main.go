3ea2ed37df2db36f7172dbc7b8b0bb57e737ad36
Mark <142676023+markluchkin@users.noreply.github.com>
2023-10-25 09:29:19 +0300

    hw1

--- main.go
+++ main.go
@@ -8,22 +8,33 @@ import (
 func main() {
 	// проверка на корректную работоспособность всех функций
 	list := LinkedList.New(5)
-	fmt.Println(list.Size()) // output 5
-	list.Add(1234)
-	list.Add(12345)
-	fmt.Println(list.Size()) // output 7
-	list.Pop()
-	fmt.Println(list.Size())            // output 6
-	fmt.Println(list.At(1), list.At(6)) // output 0 1234
-	list.Add(123456)
-	//list.Add(12323123)
-	list.DeleteFrom(6)
-	fmt.Println(list.At(6)) // output 123456
-	list.UpdateAt(0, 1111)
-	list.UpdateAt(1, 2222)
-	fmt.Println("values of list:")
+	fmt.Println(list.Size())
+	list.UpdateAt(0, 1)
+	list.UpdateAt(1, 2)
+	list.UpdateAt(2, 3)
+	list.UpdateAt(3, 4)
+	list.UpdateAt(4, 5)
+	fmt.Println(list.Size())
+	list.Add(6)
+	fmt.Println(list.Size())
 	for i := 0; i < list.Size(); i++ {
 		fmt.Printf("pos = %d, val = %d \n", i, list.At(i))
 	}
-	fmt.Println(list.Size()) //output 7
+	list.Pop()
+	fmt.Println(list.Size())
+	for i := 0; i < list.Size(); i++ {
+		fmt.Printf("pos = %d, val = %d \n", i, list.At(i))
+	}
+	//list.DeleteFrom(0)
+	list.DeleteFrom(1)
+	fmt.Println(list.Size())
+	for i := 0; i < list.Size(); i++ {
+		fmt.Printf("pos = %d, val = %d \n", i, list.At(i))
+	}
+	list.DeleteFrom(1)
+	fmt.Println(list.Size())
+	for i := 0; i < list.Size(); i++ {
+		fmt.Printf("pos = %d, val = %d \n", i, list.At(i))
+	}
+
 }
