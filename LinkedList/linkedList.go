01aa5e9a572aaa0ede538f2c0a4f91fad86b383e
Mark <142676023+markluchkin@users.noreply.github.com>
2023-10-26 16:51:49 +0300

    hw1: fixed linkedList.go

--- LinkedList/linkedList.go
+++ LinkedList/linkedList.go
@@ -1,72 +1,87 @@
 package LinkedList
 
-type Node struct {
-	Val  int
-	Next *Node
+import "fmt"
+
+type node struct {
+	val  int
+	next *node
 }
 
 type linkedList struct {
-	Head *Node
+	head *node
 }
 
 func New(q int) *linkedList {
 	// Инициализировать новый список. Количество элементов q.
-	head := Node{}
+
+	if q <= 0 {
+		fmt.Println("ERROR: list size is incorrect.")
+		return &linkedList{head: &node{}}
+	}
+
+	head := node{}
 	currNode := &head
-	lst := linkedList{Head: &head}
+	lst := linkedList{head: &head}
+
 	for i := 0; i < q-1; i++ {
-		currNode.Next = &Node{}
-		currNode = currNode.Next
+		currNode.next = &node{}
+		currNode = currNode.next
 	}
 	return &lst
 }
 
 func (l *linkedList) Add(value int) {
-	newNode := &Node{Val: value, Next: nil}
-	if l.Head == nil {
-		l.Head = newNode
-	} else {
-		curr := l.Head
-		for curr.Next != nil {
-			curr = curr.Next
-		}
-		curr.Next = newNode
+	// Добавить новый элемент в конец.
+	newNode := &node{val: value, next: nil}
+	if l.head == nil {
+		l.head = newNode
+		return
 	}
+
+	curr := l.head
+	for curr.next != nil {
+		curr = curr.next
+	}
+	curr.next = newNode
+
 }
 
 func (l *linkedList) Pop() {
 	// Удалить элемент из конца.
-	curr := l.Head
-	if curr.Next == nil {
+	curr := l.head
+	if curr.next == nil {
 		return
 	}
 
-	for curr.Next.Next != nil {
-		curr = curr.Next
+	for curr.next.next != nil {
+		curr = curr.next
 	}
 
-	curr.Next = nil
+	curr.next = nil
 }
 
 func (l *linkedList) At(pos int) int {
 	// Получение элемента из позиции pos.
-	curr := l.Head
+	if pos < 0 {
+		fmt.Println("ERORR: inputted position doesn't exist.")
+	}
+	curr := l.head
 	i := 0
-	for i != pos && curr.Next != nil {
-		curr = curr.Next
+	for i != pos && curr.next != nil {
+		curr = curr.next
 		i++
 	}
 
-	return curr.Val
+	return curr.val
 }
 
 func (l *linkedList) Size() int {
 	// Вернуть длину списка.
 	size := 0
-	curr := l.Head
-	for curr.Next != nil {
+	curr := l.head
+	for curr.next != nil {
 		size++
-		curr = curr.Next
+		curr = curr.next
 	}
 	size++
 	return size
@@ -74,32 +89,33 @@ func (l *linkedList) Size() int {
 
 func (l *linkedList) DeleteFrom(pos int) {
 	// Удалить элемент из позиции pos.
-	curr := l.Head
-	if curr.Next == nil {
+	curr := l.head
+	if curr.next == nil {
 		return
 	}
 	if pos == 0 {
-		curr = curr.Next
+		curr = curr.next
 		return
 	}
 
-	for i := 0; i != pos-1 && curr.Next != nil; i++ {
-		curr = curr.Next
+	for i := 0; i != pos-1 && curr.next != nil; i++ {
+		curr = curr.next
 	}
 
-	curr.Next = curr.Next.Next
+	curr.next = curr.next.next
 }
 
 func (l *linkedList) UpdateAt(pos, val int) {
 	// Значение на позиции pos сделать равным val.
 	if pos < 0 || pos >= l.Size() { // недопустимая позиция
+		fmt.Println("ERORR: inputted position doesn't exist.")
 		return
 	}
 
-	curr := l.Head
+	curr := l.head
 	for i := 0; i < pos; i++ {
-		curr = curr.Next
+		curr = curr.next
 	}
 
-	curr.Val = val
+	curr.val = val
 }
